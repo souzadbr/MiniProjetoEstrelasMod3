06ad48cabeac4fade9a34e386163c7eafb185b87
Bárbara Ann Lima <babiplima@gmail.com>
2021-09-29 22:15:53 -0300

    Classe Produto Modelada

--- src/br/com/zup/Produto.java
+++ src/br/com/zup/Produto.java
@@ -2,14 +2,40 @@ package br.com.zup;
 
 public class Produto {
     //Atributos
+
     private String nome;
     private double valor;
 
     //Métodos construtores
 
+    public Produto(){
 
+    }
     public Produto(String nome, double valor) {
         this.nome = nome;
         this.valor = valor;
     }
+
+    //Métodos Getters
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+    //Método StringBuilder
+
+        @Override
+        public String toString() {
+            StringBuilder retorno = new StringBuilder();
+            retorno.append("\n Nome " + nome);
+            retorno.append("\n Valor R$ " +valor);
+            retorno.append("\n\n");
+            return retorno.toString();
+
+    }
+
 }
