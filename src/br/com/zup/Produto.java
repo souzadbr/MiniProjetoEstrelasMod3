ad094d99aeb5ed482377053b65cc24ac50428aac
Bárbara Ann Lima <babiplima@gmail.com>
2021-09-29 22:03:09 -0300

    Criar classe produto

---
+++ src/br/com/zup/Produto.java
@@ -0,0 +1,15 @@
+package br.com.zup;
+
+public class Produto {
+    //Atributos
+    private String nome;
+    private double valor;
+
+    //Métodos construtores
+
+
+    public Produto(String nome, double valor) {
+        this.nome = nome;
+        this.valor = valor;
+    }
+}
