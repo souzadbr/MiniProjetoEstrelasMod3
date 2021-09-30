c4da16b704d473eb7f93db4ed246c76464f64774
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-29 22:09:43 -0300

    Criar métodos construtores da classe Pedido

--- src/br/com/zup/Pedido.java
+++ src/br/com/zup/Pedido.java
@@ -5,4 +5,14 @@ public class Pedido {
     private double numeroDaComanda;
     private double numeroDaMesa;
 
+    public Pedido() {
+
+    }
+
+    public Pedido(double numeroDaComanda, double numeroDaMesa) {
+        this.numeroDaComanda = numeroDaComanda;
+        this.numeroDaMesa = numeroDaMesa;
+    }
+
+
 }
